b5e04617e6546649140e749d2c430693b34a9853
Carl Labuschagne <carl@carllabuschagne.com>
2021-07-21 21:41:47 +0200

    update

---
+++ MVCCoreTutorial/Controllers/YoloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCCoreTutorial.Models;
+
+namespace MVCCoreTutorial.Controllers
+{
+	public class YoloController : Controller
+	{
+		public IActionResult Index()
+		{
+			Yolo yolo = new Yolo();
+
+			yolo.ID = 1;
+			yolo.YoloMessage = "Yo only live oop";
+
+			return View(yolo);
+		}
+	}
+}
---
+++ MVCCoreTutorial/Models/Yolo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCoreTutorial.Models
+{
+	public class Yolo
+	{	
+		public int ID { get; set; }
+
+		public string YoloMessage { get; set; }
+
+	}
+}
